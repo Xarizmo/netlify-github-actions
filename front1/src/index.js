2691fdcfd3426b81a671c6f4fee487ff2b5acef8
Xarizmo <mpolikarpoff@gmail.com>
2021-02-03 03:11:29 +0300

    add front1 centry release

--- front1/src/index.js
+++ front1/src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
 
 Sentry.init({
-  dsn: "https://4b721ffc5127409f94bcfb451e3a3e1d@o514138.ingest.sentry.io/5616960",
-  integrations: [
-    new Integrations.BrowserTracing(),
-  ],
-  
+  dsn:
+    "https://4b721ffc5127409f94bcfb451e3a3e1d@o514138.ingest.sentry.io/5616960",
+  release: process.env.REACT_APP_SENTRY_RELEASE,
+  integrations: [new Integrations.BrowserTracing()],
+
   // We recommend adjusting this value in production, or using tracesSampler
   // for finer control
   tracesSampleRate: 1.0,
@@ -20,5 +20,5 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
